1da7193bd4a0230ab3270ed2c57c955d79d12ab8
dpolimeni <diego.polimeni99@gmail.com>
2023-11-24 00:45:03 +0100

    add mail + schemas

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.config import configuration
+from src.database import make_session
+
 
 router = APIRouter()
 
 
 @router.post("/user-create")
-async def create_user():
+async def create_user(db: Depends(make_session)):
     pass
