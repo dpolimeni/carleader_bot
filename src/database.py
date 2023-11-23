b988356a15956404e3675358bf945b20b550d62a
dpolimeni <diego.polimeni99@gmail.com>
2023-11-24 00:30:52 +0100

    fix on db init

--- src/database.py
+++ src/database.py
@@ -2,9 +2,11 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from collections.abc import Generator
+import os
 
 
-engine = create_engine("sqlite://", echo=True)
+engine = create_engine("sqlite:///example.db", echo=True)
+session_maker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class Base(DeclarativeBase):
