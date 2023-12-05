8989ec9e0cac2980cf56217f0546e70e150f41ca
dpolimeni <diego.polimeni99@gmail.com>
2023-12-05 21:25:38 +0100

    auth tests

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -4,12 +4,24 @@ from typing import Annotated
 from src.config import configuration
 from src.database import make_session
 from src.auth.models import User
+from src.auth.schemas import UserCreate
 
 
 router = APIRouter()
 
 
-@router.get("/user-create")
-async def create_user(db: Annotated[Session, Depends(make_session)]):
-    print(db.query(User).all())
-    return {"test": "test"}
+@router.post("/user-create")
+async def create_user(db: Annotated[Session, Depends(make_session)], user: UserCreate):
+    new_user = User(
+        username=user.username,
+        hashed_password=user.password,
+        email=user.email,
+    )
+    db.add(new_user)
+
+    # Commit the transaction
+    db.commit()
+
+    # Close the session
+    db.close()
+    return {"response": "user created"}
